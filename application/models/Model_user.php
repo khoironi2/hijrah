57d8b0cba4b6f958a33bdd454895d57c46cb4bc3
khoironi2 <khoironi7887@students.amikom.ac.id>
2019-10-31 12:03:30 +0700

    penambahan rest server

--- application/models/Model_user.php
+++ application/models/Model_user.php
@@ -11,6 +11,25 @@ class Model_user extends CI_Model {
 			return false;
 	}
 
+	public function getData() {
+		$this->db->select('*');
+		$this->db->from('tbl_user');
+		
+		$result = $this->db->get();
+
+		return $result->result();
+	}
+
+	function getDataById($id) {
+		$this->db->select('*');
+		$this->db->from('tbl_user');
+		$this->db->where('id', $id);
+
+		$result = $this->db->get();
+
+		return $result->row();
+	}
+
 	function getLogin($email, $password) {
 		$this->db->select('*');
 		$this->db->from('tbl_user');
@@ -25,6 +44,23 @@ class Model_user extends CI_Model {
         $this->db->where('email', $email);
         $this->db->update('tbl_user', $data);
     }
+    
+    function updateData($id, $data) {
+		$this->db->where('id', $id);
+		$this->db->update('tbl_user', $data);
+	}
+
+	
+
+	public function deleteData($id) {
+		$this->db->where('id', $id);
+		$this->db->delete('tbl_user');
+
+		if($this->db->affected_rows() > 0)
+			return true;
+		else
+			return false;
+	}
 
 
 	
